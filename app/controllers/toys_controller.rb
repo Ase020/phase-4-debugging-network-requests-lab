cd6dcf90a87d88fc2cf70adfa6eed4b80132e61e
felixNyalenda <felixasejunior@gmail.com>
2023-06-21 12:41:39 +0300

    Completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -3,23 +3,28 @@ class ToysController < ApplicationController
 
   def index
     toys = Toy.all
-    render json: toys
+    render json: toys, except: [:created_at, :updated_at]
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.create(toy_params)
+    render json: toy, except: [:created_at, :updated_at], status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy, except: [:created_at, :updated_at], status: :ok
+  rescue ApplicationRecord::RecordNotFound
+    render_not_found_response
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
+  rescue ApplicationRecord::RecordNotFound
+    render_not_found_response
   end
 
   private
@@ -28,4 +33,11 @@ class ToysController < ApplicationController
     params.permit(:name, :image, :likes)
   end
 
+  def find_toy
+    Toy.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "toy not found" }, status: :not_found
+  end
 end
